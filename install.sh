1c143eedc85df0628e779a9eb10b29178e3fb2cf
Matt Westcott <matt@west.co.tt>
2013-10-08 22:02:44 +0100

    further install steps

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-sudo apt-get install python-dev libasound-dev
+sudo apt-get install -y python-dev libasound-dev python-pip supervisor
 git clone https://github.com/superquadratic/rtmidi-python.git
 cd rtmidi-python
 python setup.py build
 cd ..
 cp rtmidi-python/build/lib.linux-armv6l-2.7/rtmidi_python.so .
+
+sudo pip install virtualenvwrapper
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv cloxhack
+pip install -r requirements.txt
